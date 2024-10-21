e58abbd295b7dd2b868d6d931ced4ec159c4cb49
gagan-sethi <ravisethi7102@gmail.com>
2024-10-21 10:58:13 +0530

    First Commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCB7lI_C38HcpzQe1WnX9HXQ3z8ENeadHk",
+    authDomain: "cap917-f5b8a.firebaseapp.com",
+    projectId: "cap917-f5b8a",
+    storageBucket: "cap917-f5b8a.appspot.com",
+    messagingSenderId: "30100",
+    appId: "1:481007566764:web:5e42a7ab8f9b2aefef8409"
+  }
+};
